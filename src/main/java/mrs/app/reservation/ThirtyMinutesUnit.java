8d8799c1919e85da017ee2dd1d4ec02f43d74bc7
1noseA <1noseA2L@gmail.com>
2021-05-05 13:42:14 +0900

    サンプルコピペによりコンパイルエラー解消

--- src/main/java/mrs/app/reservation/ThirtyMinutesUnit.java
+++ src/main/java/mrs/app/reservation/ThirtyMinutesUnit.java
@@ -1,5 +1,8 @@
 package mrs.app.reservation;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -12,17 +15,16 @@ import javax.validation.Payload;
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 @Retention(RUNTIME)
 public @interface ThirtyMinutesUnit {
-	// {メッセージキー}形式にすることでメッセージはプロパティファイルから取得される
-	String message() default "mrs.app.reservation.ThirtyMinutesUnit.message}";
+	String message() default "{mrs.app.reservation.ThirtyMinutesUnit.message}";
 
-	Class<?>[]groups() default {};
+	Class<?>[] groups() default {};
 
-	Class<? extends Payload>[]payload() default {};
+	Class<? extends Payload>[] payload() default {};
 
 	@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
 	@Retention(RUNTIME)
 	@Documented
 	public @interface List {
-		ThirtyMinutesUnit[]values();
+		ThirtyMinutesUnit[] value();
 	}
 }
